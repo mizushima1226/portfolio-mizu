e4a026355d7e87a91818363bd0f90ece15c7a2a1
mizushima1226 <mizuki.bz.3224@gmail.com>
2020-05-07 21:05:12 +0900

    reactstrap

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
-
+import { Container } from 'reactstrap';
 class Index extends React.Component {
 	render() {
 		return (
 			<BaseLayout>
-				<h1>I am Index Page!</h1>
+				<Container>
+					<h1>I am Index Page!</h1>
+				</Container>
 			</BaseLayout>
 		);
 	}
